3555422c8be48c29cf4580ee222a9ff8094408b1
TolLescnq7 <122276538+TolLescnq7@users.noreply.github.com>
2024-11-03 14:21:02 +0800

    Update MyApp.kt

--- buildSrc/src/main/java/MyApp.kt
+++ buildSrc/src/main/java/MyApp.kt
@@ -1,7 +1,7 @@
 @file:Suppress("unused")
 
 object MyApp {
-    const val appId = "dev.jahir.blueprint.app"
+    const val appId = "app.aqcpol.pixelized.adaptive"
     const val version = 100
     const val versionName = "1.0.0"
 }
